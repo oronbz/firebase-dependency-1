d491769acb54f4abf17139bba5111e7cc64f0047
Oron Ben Zvi <oronbz@gmail.com>
2017-12-20 13:21:07 +0200

    Initial Commit

---
+++ FirebaseDependency1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseDependency1
+//
+//  Created by Oron Ben Zvi on 12/20/17.
+//  Copyright © 2017 Oron Ben Zvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
